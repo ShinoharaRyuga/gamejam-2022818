7fe75bc4cb870d040e0498fce88538733943493b
SinoharaRyuga <raitan3170@gmail.com>
2022-08-18 15:33:02 +0900

    フェードインを作成

--- Assets/Hisitani/GameManager.cs
+++ Assets/Hisitani/GameManager.cs
@@ -5,40 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance = default;
 
-    [SerializeField] int nokori;
-    [SerializeField] int noruma;
-    [SerializeField] string clearscene;
-    [SerializeField] string gameoverscene;
-    bool game;
+    [SerializeField] int _nokori;
+    [SerializeField] int _noruma;
+    [SerializeField] string _clearscene;
+    [SerializeField] string _gameoverscene;
+    [SerializeField] Fade _fadeInPrefab = default;
+    [SerializeField] Fade _fadeOutPrefab = default;
 
-    public int Noruma { get => noruma; set => noruma = value; }
-    public int Nokori { get => nokori; set => nokori = value; }
+    bool _game = false;
 
-    void Start()
+    public int Noruma { get => _noruma; set => _noruma = value; }
+    public int Nokori { get => _nokori; set => _nokori = value; }
+
+    private void Awake()
     {
-        
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            SceneManager.sceneLoaded += GameSceneLoad;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(game)
+        if(_game)
         {
-            if (nokori < 0)
+            if (_nokori < 0)
             {
                 GameClear();
             }
         }
-    
     }
+
+    public void GameSceneChange()
+    {
+        Instantiate(_fadeInPrefab);
+    }
+
+    void GameSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            Instantiate(_fadeOutPrefab);
+        }
+    }
+
     void GameClear()
     {
-        SceneManager.LoadScene(clearscene);
+        SceneManager.LoadScene(_clearscene);
     }
     void GameOver()
     {
-        SceneManager.LoadScene(gameoverscene);
+        SceneManager.LoadScene(_gameoverscene);
     }
 }
--- Assets/Shinohara/Fade.cs
+++ Assets/Shinohara/Fade.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fade : MonoBehaviour
 {
     public void ThisDestroy()
     {
         Destroy(gameObject);
+    }
+
+    public void GameSceneChage()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
