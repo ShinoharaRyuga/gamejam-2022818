79649452b240afa02c86a42a2b525d34b889af4d
sinzitoyama <yamat422@gmail.com>
2022-08-18 13:33:10 +0900

    剣のスタミナ

---
+++ Assets/Toyama/SwordStamina.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordStamina : MonoBehaviour
+{
+    [SerializeField] float _maxStamina = 0f;
+    [SerializeField] float _currentStamina = 0f;
+    [SerializeField] float _time = 0f;
+    [SerializeField] float _damage = 1f;
+    [SerializeField] float _nowTime;
+
+    void Start()
+    {
+        _currentStamina = _maxStamina;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+
+        if (_nowTime < _time)
+        {
+            _currentStamina -= _damage;
+            Debug.Log(_currentStamina);
+            _time = 0f;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
