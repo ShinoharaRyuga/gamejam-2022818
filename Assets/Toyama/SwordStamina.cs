67328d649ccc703e5cb895dcc44da7207aa24e93
SinoharaRyuga <raitan3170@gmail.com>
2022-08-20 21:58:22 +0900

    値を調節した

--- Assets/Toyama/SwordStamina.cs
+++ Assets/Toyama/SwordStamina.cs
@@ -9,15 +9,20 @@ public class SwordStamina : MonoBehaviour
     [SerializeField] float _addStamina = 1f;
     [SerializeField] float _damage = 1f;
     [SerializeField] float _reduceTime = 1f;
-    [SerializeField] Slider sli;
+    Slider sli;
 
     bool _isFall = true;
     bool _dead = false;
     float _currentStamina = 0f;
     float _time = 0f;
     GameManager _gameManager = default;
+
+    public float CurrentStamina { get => _currentStamina; set => _currentStamina = value; }
+    public bool Dead { get => _dead; set => _dead = value; }
+
     void Start()
     {
+        sli = GameObject.Find("SwordStamina").GetComponent<Slider>();
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         _currentStamina = _maxStamina;
         sli.value = 1;
@@ -30,6 +35,8 @@ public class SwordStamina : MonoBehaviour
             if (_currentStamina < _maxStamina)
             {
                 _currentStamina += _addStamina;
+                sli.value = (float)_currentStamina / (float)_maxStamina;
+               
             }
         }
        
@@ -45,9 +52,18 @@ public class SwordStamina : MonoBehaviour
             }
             else if(_currentStamina <= 0 && !_dead)
             {
+                Debug.Log("hit");
                 _dead = true;
                 _gameManager.GameOver();
             }
+
+            if (_gameManager.Nokori == 1)
+            {
+                _damage = 12.5f;
+                _addStamina = 1;
+            }
         }
+
+        Debug.Log(_currentStamina);
     }
 }
